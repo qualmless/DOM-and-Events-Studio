d38c84e37bf011a7c54ac7fec0cb8f40894d4ed5
qualmless <terasmash@gmail.com>
2019-12-17 11:25:49 -0600

    updated scripts

--- scripts.js
+++ scripts.js
@@ -1,4 +1,29 @@
 // Write your JavaScript code here.
+function init () {
+    let button = document.getElementById("takeoff");
+    let newFlightStatus = document.getElementById("flightStatus");
+
+    console.log("jfklajfdlak",button);
+
+
+    // Put your code for the exercises here.
+    button.addEventListener("click", function() {
+        let shuttleReady = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (shuttleReady === true) {
+            newFlightStatus.innerHTML = "Shuttle in flight.";
+        } 
+        // else {
+
+        // }
+
+    })
+}
+
+window.onload = init;
+
+
+
+
 
 
 
